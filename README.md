25ab4608cb1a55c6b745d776695f120530380687
dorianpc <dorianpc@gmail.com>
2020-02-18 14:24:12 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# .NET Core and the CLI
+
+CLI to build a web service, unit tests, and a console application.
+Used .NET Core built-in dependency container
+
+This solution is for educational purposes. 
---
+++ person-api/Controllers/PeopleController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace person_api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PeopleController : ControllerBase
+    {
+        IPeopleProvider _provider;
+
+        public PeopleController(IPeopleProvider provider)
+        {
+            this._provider = provider;
+        }
+
+        [HttpGet]
+        public IEnumerable<Person> Get()
+        {
+            return _provider.GetPeople();
+        }
+
+        [HttpGet("{id}")]
+        public Person Get(int id)
+        {
+            try
+            {
+                return _provider.GetPerson(id);
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+    }
+}
